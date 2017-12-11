bab3ec68f2e541545536e200d8d1c81589c35c85
Andy Kessler <akkessler@users.noreply.github.com>
2017-12-11 17:01:50 -0600

    Refactor code to prepare for 3.2

--- src/main/java/io/akessler/day3/Day3.java
+++ src/main/java/io/akessler/day3/Day3.java
@@ -5,9 +5,16 @@ import io.akessler.AdventUtility;
 public class Day3 {
 
     public static void main(String[] args) {
-
         int inputValue = Integer.parseInt(AdventUtility.readInput(3).get(0));
 
+        int answer1 = manhattenDistance(inputValue);
+        int answer2 = firstValueLargerThan(inputValue);
+
+        System.out.println(answer1);
+        System.out.println(answer2);
+    }
+
+    private static int manhattenDistance(int inputValue) {
         int x=1;
         while(Math.pow(x,2)<=inputValue) {
             x+=2;
@@ -16,14 +23,8 @@ public class Day3 {
 
         int minRange = (int) Math.pow(x,2) + 1;
         int maxRange = (int) Math.pow(x+2,2);
-        System.out.println(minRange);
-        System.out.println(maxRange);
-
         int diff = (maxRange - minRange) + 1;
-        System.out.println(diff);
-
         int sideLength = diff / 4;
-        System.out.println(sideLength);
 
         int dir = 0;
         for(int i=1; i<=4; i++) {
@@ -35,16 +36,14 @@ public class Day3 {
 
         int dist = (x+2) / 2; // integer division
         int start = minRange + (sideLength * (dir - 1));
-        int end = start + (sideLength - 1); // -1 cause we include start
-
-        System.out.println(dist);
-        System.out.println(start);
-        System.out.println(end);
-
+//        int end = start + (sideLength - 1); // -1 cause we include start
         int mid = start + (dist - 1);
         dist += Math.abs(inputValue - mid);
-        System.out.println(dist);
 
-        // start part 2
+        return dist;
+    }
+
+    private static int firstValueLargerThan(int inputValue) {
+        return -1;
     }
 }
